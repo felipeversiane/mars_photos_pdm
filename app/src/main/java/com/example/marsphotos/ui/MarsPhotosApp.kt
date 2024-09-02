70b564b91c01b2a87d60940585a78e3d438cee2e
Felipe Fernandes Versiane <felipeversiane09@gmail.com>
2024-09-01 22:30:23 -0300

    adding request

--- app/src/main/java/com/example/marsphotos/ui/MarsPhotosApp.kt
+++ app/src/main/java/com/example/marsphotos/ui/MarsPhotosApp.kt
@@ -46,10 +46,12 @@ fun MarsPhotosApp() {
         Surface(
             modifier = Modifier.fillMaxSize()
         ) {
-            val marsViewModel: MarsViewModel = viewModel()
+            val marsViewModel: MarsViewModel =
+                viewModel(factory = MarsViewModel.Factory)
             HomeScreen(
                 marsUiState = marsViewModel.marsUiState,
-                contentPadding = it,
+                retryAction = marsViewModel::getMarsPhotos,
+                contentPadding = it
             )
         }
     }
